2723d5468f26adfbb45a87d641a1f47d90ea9a48
Alan Vidal <alanfonseca7812@gmail.com>
2020-10-07 12:06:16 -0300

    Ajustes

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const dotenv = require('dotenv').config()
 
 
 //Body-Parser
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.use(bodyParser.json())
 
 
@@ -35,11 +37,14 @@ app.get("/contato", ((req, res) => {
 
 //Config Nodemailer
 
-const {USER,PASS} = process.env
+const {
+    USER,
+    PASS
+} = process.env
 const config = {
-    host:  'smtp.umbler.com',
+    host: 'smtp.umbler.com',
     port: 587,
-    auth:{
+    auth: {
         user: USER,
         pass: PASS,
     }
@@ -49,35 +54,34 @@ const transporter = nodemailer.createTransport(config)
 
 //Router E-mail
 
-app.post("/send",(req,res) => {
+app.post("/send", (req, res) => {
 
     const name = req.body.name
     const email = req.body.email
     const phone = req.body.phone
     const text = req.body.message
-    
+
     const message = {
         from: USER,
         to: USER,
         replyTo: email,
         text: "Nome: " + name + "\n\n" +
-         "Email: " + email + "\n\n" +
-         "telefone: " + phone + "\n\n" +
-         "Mensagem: " + text
+            "Email: " + email + "\n\n" +
+            "telefone: " + phone + "\n\n" +
+            "Mensagem: " + text
     }
     transporter.sendMail(message, function (error, info) {
-    
-        if (error) {     
-         return res.sendFile(__dirname + "/views/erroaoenviar.html")
-        } 
+
+        if (error) {
+            return res.sendFile(__dirname + "/views/erroaoenviar.html")
+        }
         return res.sendFile(__dirname + "/views/enviado.html")
-       
+
     })
 })
 
 //configurando a porta do servidor
 
-app.listen(process.env.PORT || 3000,() => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor rodando na porta: " + 3000)
-    
 })
